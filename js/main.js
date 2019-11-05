3be095b054826a4263f6716a19711e2a361d701a
vsmaslov <50521620+vsmaslov@users.noreply.github.com>
2019-11-05 12:17:45 +0700

    исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
-$(function () {
-  $('.sidebar__link-box').click(function () {
-    $('.sidebar__gamburger').toggleClass('sidebar__link_active');
-    $('.sidebar').toggleClass('sidebar_active');
-    $('.menu__item a').toggleClass('sidebar__a_active');
-    $('.menu__item').toggleClass('menu__item_mobil-active');
+$(function() {
+  $(".sidebar__link-box").click(function() {
+    $(".sidebar__gamburger").toggleClass("sidebar__link_active");
+    $(".sidebar").toggleClass("sidebar_active");
+    $(".menu >li a").toggleClass("sidebar__a_active");
+    $(".menu > li").toggleClass("menu__item_mobil-active");
   });
 
-  $('.menu__item').click(function () {
-
-    $('.menu__item_active').removeClass('menu__item_active');
-    $(this).addClass('menu__item_active');
-
-  })
-
-})
+  $(".menu__item").click(function() {
+    let i = 1;
+    for (i; i < 6; i++) {
+      $(".menu > li").addClass("menu__item");
+      $(".menu > li").removeClass("menu__item-" + i);
+    }
+    $number = $(this).attr("data-number");
+    $(this).addClass("menu__item-" + $number);
+    $(this).toggleClass("menu__item");
+    $(".menu__item_active").removeClass("menu__item_active");
+    $(this).addClass("menu__item_active");
+  });
+});
